e95434a5efe7cb48dcac6eccef32151131bb7019
adarsht00001 <adarsht00001@gmail.com>
2022-12-27 13:40:03 +0530

    added search

--- front-end/src/components/Table/table.js
+++ front-end/src/components/Table/table.js
@@ -2,15 +2,36 @@ import { useEffect, useState } from "react";
 import Container from "react-bootstrap/esm/Container";
 import Table from "react-bootstrap/Table";
 import axios from "../../axios/axios";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 function UserTable() {
-  const [user,setUser]=useState([])
-  useEffect(()=>{
-    axios.get('/userlist').then((response)=>{
-      setUser(response.data.result)
-    })
-  },[])
+  const [user, setUser] = useState([]);
+  const [search,setSearch]=useState('')
+  useEffect(() => {
+    axios.get("/userlist").then((response) => {
+      setUser(response.data.result);
+    });
+  }, []);
   return (
-    <Container className="p-5">
+    <Container>
+      <Form className="d-flex p-5">
+        <Form.Control
+          type="search"
+          placeholder="Search"
+          className="me-2 w-50"
+          size="sm"
+          aria-label="Search"
+          value={search}
+          onChange={(e)=>setSearch(e.target.value)}
+        />
+        <Button className="float-end pr-5"
+          onClick={() => {
+          }}
+          variant="outline-success"
+        >
+          Add User
+        </Button>
+      </Form>
       <Table striped bordered hover>
         <thead>
           <tr>
@@ -21,15 +42,23 @@ function UserTable() {
           </tr>
         </thead>
         <tbody>
-          {user.map((e,i) => {
-            return(
-          <tr key={i}>
-            <td>{i+1}</td>
-            <td>{e.name}</td>
-            <td>{e.email}</td>
-            <td></td>
-          </tr>
-            )
+          {user.filter((value) => {
+              if (search === "") {
+                return value;
+              } else if (
+                value.name.toLowerCase().includes(search.toLowerCase())
+              ) {
+                return value;
+              }
+            }).map((e, i) => {
+            return (
+              <tr key={i}>
+                <td>{i + 1}</td>
+                <td>{e.name}</td>
+                <td>{e.email}</td>
+                <td></td>
+              </tr>
+            );
           })}
         </tbody>
       </Table>
